092abcd9583c7dd7a0384cf9a7238e9be4ea1be2
Aldycky1 <bagusaldycky@gmail.com>
2021-08-17 17:58:35 +0700

    created

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row header my-5 p-3">
+        <div class="col-lg-12">
+          <h2>What I do today?</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <div class="row">
+              <div
+                v-for="tugasSaya in tugas"
+                :key="tugasSaya.id"
+                class="col-lg-12 mb-3 text-left tugas"
+              >
+                <div class="form-check">
+                  <ItemTugas v-bind:tugasSaya="tugasSaya" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+
+import ItemTugas from "./components/ItemTugas.vue";
 
 export default {
-  name: 'App',
+  name: "app",
+  data: function() {
+    return {
+      tugas: [
+        {
+          id: 1,
+          deskripsi: "Melanjutkan tugas projek UAS",
+          isDone: false,
+        },
+        {
+          id: 2,
+          deskripsi: "Mengerjakan projek website laravel dan vue js",
+          isDone: false,
+        },
+        {
+          id: 3,
+          deskripsi: "Menonton film dan anime",
+          isDone: false,
+        },
+        {
+          id: 4,
+          deskripsi: "Makan 3 kali sehari",
+          isDone: false,
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ItemTugas,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +73,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.header {
+  background: #1abc9c;
+}
+
+.header h2 {
+  margin: 0;
+}
+
+.tugas .item {
+  font-size: 18px;
+}
+
+.item-completed {
+  color: #1abc9c;
 }
 </style>
---
+++ src/components/ItemTugas.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <input
+      class="form-check-input"
+      onclick=""
+      type="checkbox"
+      value=""
+      id="defaultCheck1"
+    />
+    <label
+      class="form-check-label item"
+      :class="{ 'item-completed': tugasSaya.isDone }"
+      for="defaultCheck1"
+    >
+      {{ tugasSaya.deskripsi }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemTugas",
+  props: ["tugasSaya"],
+};
+</script>
+
+<style scoped></style>
